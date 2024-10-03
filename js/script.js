e180535b8bc81e427083fadc5495506ae884407a
mrdarip <darioelas2005@gmail.com>
2024-10-03 10:42:46 +0200

    fixed conversion, implemented scaling output gcode and displaying max and min values

--- js/script.js
+++ js/script.js
@@ -6,25 +6,66 @@ offCommand = document.getElementById('offCommand');
 offSpeed = document.getElementById('offSpeed');
 onSpeed = document.getElementById('onSpeed');
 
+scaleInput = document.getElementById('scale');
+
+minXSpan = document.getElementById('minX');
+minYSpan = document.getElementById('minY');
+maxXSpan = document.getElementById('maxX');
+maxYSpan = document.getElementById('maxY');
+
 convertBtn.addEventListener('click', function() {
     var polyline = input.value;
-    var gcode = polylineToGcode(polyline);
+    var gcodeAndStats = polylineToGcode(polyline);
+
+    var gcode = gcodeAndStats[0];
+    var maxX = gcodeAndStats[1];
+    var maxY = gcodeAndStats[2];
+    var minX = gcodeAndStats[3];
+    var minY = gcodeAndStats[4];
+
+    minXSpan.innerHTML = minX;
+    minYSpan.innerHTML = minY;
+    maxXSpan.innerHTML = maxX;
+    maxYSpan.innerHTML = maxY;
+
     output.value = gcode;
 });
 
 
 function polylineToGcode(polyline) {
     gcode = "";
+    maxX = -Infinity;
+    maxY = -Infinity;
+    minX = Infinity;
+    minY = Infinity;
+
     polyline = JSON.parse(polyline);
+    console.log(polyline);
 
     gcode += offCommand.value + "\n";
     polyline.forEach(path => {
-        gcode += "G0 X" + path[0][0] + " Y" + path[0][1] + "\n"; //go to first path point
+        gcode += "G0 X" + path[0] + " Y" + path[1] + "\n"; //go to first path point
         gcode += "G0 F" + onSpeed.value + "\n";
         gcode += onCommand.value + "\n";
         
         path.forEach(point => {
-            gcode += "G1 X" + point[0] + " Y" + point[1] + "\n";
+            pointX = point[0] * scaleInput.value;
+            pointY = point[1] * scaleInput.value;
+
+            gcode += "G1 X" + pointX + " Y" + pointY + "\n";
+
+            if(pointX > maxX) {
+                maxX = pointX;
+            }
+            if(pointY > maxY) {
+                maxY = pointY;
+            }
+            if(pointX < minX) {
+                minX = pointX;
+            }
+            if(pointY < minY) {
+                minY = pointY;
+            }
         });
 
         gcode += offCommand.value + "\n";
@@ -32,5 +73,5 @@ function polylineToGcode(polyline) {
     });
     //todo:: go gome
 
-    return gcode;
+    return [gcode,maxX,maxY,minX,minY];
 }
